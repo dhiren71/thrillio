7aa022d8bd241095f52c66229fba8b6b25074155
Dhiren Patel <talk2_dhiren@yahoo.com>
2022-11-30 18:59:52 -0500

    Added com.semanticsquare.thrillio.constants packages and classes

---
+++ src/com/semanticsquare/thrillio/constants/UserType.java
@@ -0,0 +1,10 @@
+package com.semanticsquare.thrillio.constants;
+
+public class UserType {
+	private UserType() {
+	}
+
+	public static final String USER = "user";
+	public static final String EDITOR = "editer";
+	public static final String CHEIFEDITOR = "chiefeditor";
+}
